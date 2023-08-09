f4f897f4ef6be7c40cccef157899cb7f252626a5
Yashin <yashinnaran@gmail.com>
2023-08-09 18:48:24 +0200

    Added OrderPlan.js component to the main OrderSummary.js component

--- src/Components/OrderPlan.js
+++ src/Components/OrderPlan.js
@@ -1,8 +1,23 @@
+import IconMusic from "../Svg_Components/IconMusic";
+import "./OrderPlan.css";
+
 const OrderPlan = () => {
   return (
-    <div className=" container p-0 order-box">
+    <div className=" container p-4 order-box">
       <div className="container p-0 order-content">
-        <div className="plan-type"></div>
+        <div className="plan-container">
+          <div className="plan-wrapper">
+            <IconMusic />
+            <div className="plan-details">
+              <span className="plan-type">Annual Plan</span>
+              <span className="price">$59.99/year</span>
+            </div>
+          </div>
+        </div>
+
+        <a className="change-tag" href="#">
+          change
+        </a>
       </div>
     </div>
   );
