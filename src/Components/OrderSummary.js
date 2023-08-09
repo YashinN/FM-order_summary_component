3583c2493898c67cec6f3ad7dabb4beb01160dbf
Yashin <yashinnaran@gmail.com>
2023-08-09 16:08:38 +0200

    Svg files component folder created

--- src/Components/OrderSummary.js
+++ src/Components/OrderSummary.js
@@ -1,7 +1,13 @@
 import "./OrderSummary.css";
 
 const OrderSummary = () => {
-  return <div className="container-fluid bg-container"></div>;
+  return (
+    <div className="container-fluid bg-container">
+      <div className="container summary-card">
+        <img src="/images/" alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default OrderSummary;
