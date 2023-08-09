8b8989900a3fd0b84edff8b75c224f358794d2a3
Yashin <yashinnaran@gmail.com>
2023-08-09 17:10:11 +0200

    Card title and paragraph content added

--- src/Components/OrderSummary.js
+++ src/Components/OrderSummary.js
@@ -4,8 +4,15 @@ import HeroIllustration from "../Svg_Components/HeroIllustration";
 const OrderSummary = () => {
   return (
     <div className="container-fluid bg-container">
-      <div className="container summary-card">
+      <div className="container summary-card p-0">
         <HeroIllustration />
+        <div className="container summary-details">
+          <h1 className="text-center title">Order Summary</h1>
+          <p className="text-center text">
+            You can now listen to millions of songs, audiobooks, and podcasts on
+            any device anywhere you like!
+          </p>
+        </div>
       </div>
     </div>
   );
