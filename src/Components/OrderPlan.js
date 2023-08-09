af3e45f1d92220753df47d9d132c8e440c2fdd29
Yashin <yashinnaran@gmail.com>
2023-08-09 17:18:09 +0200

    New sub compmonent for order summary created

---
+++ src/Components/OrderPlan.js
@@ -0,0 +1,11 @@
+const OrderPlan = () => {
+  return (
+    <div className=" container p-0 order-box">
+      <div className="container p-0 order-content">
+        <div className="plan-type"></div>
+      </div>
+    </div>
+  );
+};
+
+export default OrderPlan;
