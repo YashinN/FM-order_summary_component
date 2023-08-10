e355c3c29ef186d1dd6ac2f3e7203e42228a159f
Yashin <yashinnaran@gmail.com>
2023-08-10 18:47:11 +0200

    Removed HeroIllustration.js component

--- src/Components/OrderSummary.js
+++ src/Components/OrderSummary.js
@@ -6,8 +6,7 @@ const OrderSummary = () => {
   return (
     <div className="container-fluid bg-container">
       <div className="container summary-card p-0">
-        <HeroIllustration />
-        <div className="container summary-details">
+        <div className="container summary-details ">
           <h1 className="text-center title">Order Summary</h1>
           <p className="text-center text">
             You can now listen to millions of songs, audiobooks, and podcasts on
